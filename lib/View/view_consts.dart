625ba83c7eead8e339eaadda33a20820c95f9ad7
muhsinabdil <muhsinabdil@hotmail.com>
2024-02-16 20:28:38 +0300

    edited users view

--- lib/View/view_consts.dart
+++ lib/View/view_consts.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 const kDefaultRadius = 10.0;
 const kDefaultPadding = 10.0;
 const kDefaultListColor = Colors.white70;
-const kDefaultTextColor = Colors.black54;
+const kDefaultTextColor = Colors.black87;
 const kSecondTextColor = Colors.deepPurple;
 const kSecondIconColor = Colors.deepPurple;
 const kThirtTextColor = Colors.red;
