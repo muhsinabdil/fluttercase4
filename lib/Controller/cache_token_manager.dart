b2b67a7af745f2d9a9db425251c6c5d7cdc830be
muhsinabdil <muhsinabdil@hotmail.com>
2024-02-16 12:53:01 +0300

    created cache token manager

---
+++ lib/Controller/cache_token_manager.dart
@@ -0,0 +1,13 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+mixin CacheTokenManager {
+  Future<bool> saveToken(String token) async {
+    //! gelen token locale kaydedilecek
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString(
+        CacheKey.TOKEN.toString(), token); //! TOKEN keyi ile kaydedildi
+    return true;
+  }
+}
+
+enum CacheKey { TOKEN }
